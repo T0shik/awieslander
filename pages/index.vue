5da7adbc7f2c0031dd7faf367d64112f7847c741
Anton Wieslander <Anton.Wieslander@nobiauk.com>
2019-03-15 09:55:25 +0000

    bobo

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6></v-flex>
-  </v-layout>
+  <div v-resize="onResize">
+    <v-parallax
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      :height="windowSize.y"
+    >
+      <v-layout align-center column justify-center>
+        <h1 class="display-2 font-weight-thin mb-3">Vuetify.js</h1>
+        <h4 class="subheading">Build your application today!</h4>
+      </v-layout>
+    </v-parallax>
+  </div>
 </template>
 
 <script>
 export default {
-  components: {}
+  computed: {},
+  data: () => ({
+    windowSize: {
+      x: 0,
+      y: 0
+    }
+  }),
+  mounted() {
+    this.onResize()
+  },
+  methods: {
+    onResize() {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight }
+    }
+  }
 }
 </script>
+
